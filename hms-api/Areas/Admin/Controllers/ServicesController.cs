2d8ccb29277d66e3568f2ca5a157cf302dbda3e5
Davison Nzeh <davidson.nzeh@tenece.com>
2020-10-27 12:26:38 +0100

    Service Category done

--- hms-api/Areas/Admin/Controllers/ServicesController.cs
+++ hms-api/Areas/Admin/Controllers/ServicesController.cs
@@ -9,18 +9,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HMS.Areas.Admin.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/admin/")]
     [ApiController]
     public class ServicesController : ControllerBase
     {
         private readonly IServices _serviceRepo;
+        private readonly IServiceCategory _serviceCategoryRepo;
 
-        public ServicesController(IServices serviceRepo)
+        public ServicesController(IServices serviceRepo, IServiceCategory serviceCategoryRepo)
         {
             _serviceRepo = serviceRepo;
+            _serviceCategoryRepo = serviceCategoryRepo;
         }
 
-        [HttpGet("Index")]
+        [HttpGet("Services")]
         public async Task<IActionResult> Services()
         {
             var services = await _serviceRepo.GetAllService();
@@ -57,8 +59,95 @@ namespace HMS.Areas.Admin.Controllers
             });
         }
 
-        //public async Task<IActionResult> GetService(){
-            
-        //}
+        [HttpGet("Service/{Id}")]
+        public async Task<IActionResult> GetService(string Id)
+        {
+            if (string.IsNullOrEmpty(Id))
+                return BadRequest(new
+                {
+                    response = 301,
+                    message = "Invalid request, Check Id and try again"
+                });
+
+            var service = await _serviceRepo.GetById(Id);
+            if(service == null)
+            {
+                return NotFound(new
+                {
+                    response = 401,
+                    message = "Service not found, wrong Id"
+                });
+            }
+
+            return Ok(new {
+            service,
+            message="service fetched"});
+        }
+
+
+        [HttpGet("ServiceCategories")]
+        public async Task<IActionResult> ServicesCategory()
+        {
+            var services = await _serviceCategoryRepo.GetCategoriesAsync();
+
+            if (services.Any())
+                return Ok(services);
+            else
+                return NoContent();
+        }
+
+
+        [HttpPost("AddServiceCategory")]
+        public async Task<IActionResult> CreateServiceCategory(ServiceCategoryDtoForCreate categoryDtoForCreate)
+        {
+            if (categoryDtoForCreate == null)
+                return BadRequest(new
+                {
+                    response = 301,
+                    message = "Invalid request"
+                });
+
+            var result = await _serviceCategoryRepo.AddServiceCategoryAsync(categoryDtoForCreate);
+            if (!result)
+            {
+                return BadRequest(new
+                {
+                    response = 501,
+                    message = "Service Category failed to create"
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Service Category created successfully"
+            });
+        }
+
+        [HttpGet("ServiceCategory/{Id}")]
+        public async Task<IActionResult> GetServiceCategory(string Id)
+        {
+            if (string.IsNullOrEmpty(Id))
+                return BadRequest(new
+                {
+                    response = 301,
+                    message = "Invalid request, Check Id and try again"
+                });
+
+            var service = await _serviceCategoryRepo.GetServiceCategoryAsync(Id);
+            if (service == null)
+            {
+                return NotFound(new
+                {
+                    response = 401,
+                    message = "Service Category not found, wrong Id"
+                });
+            }
+
+            return Ok(new
+            {
+                service,
+                message = "service Category fetched"
+            });
+        }
     }
 }
---
+++ hms-api/Areas/Admin/Repositories/ServiceCategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using HMS.Areas.Admin.Dtos;
+using HMS.Areas.Admin.Interfaces;
+using HMS.Areas.Admin.Models;
+using HMS.Database;
+using HMS.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMS.Areas.Admin.Repositories
+{
+    public class ServiceCategoryRepository : GenericRepository<ServiceCategory>, IServiceCategory
+    {
+        private readonly IMapper _mapper;
+
+        public ServiceCategoryRepository(ApplicationDbContext context, IMapper mapper)
+            :base(context)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<bool> AddServiceCategoryAsync(ServiceCategoryDtoForCreate serviceCategory)
+        {
+            if (serviceCategory == null)
+                return false;
+
+            var categoryToAdd = _mapper.Map<ServiceCategory>(serviceCategory);
+
+            var res = await Insert(categoryToAdd);
+            return res;            
+        }
+
+        public async Task<IEnumerable<ServiceCategoryDtoForView>> GetCategoriesAsync()
+        {
+            var categories = await Get();
+
+            return _mapper.Map<IEnumerable<ServiceCategoryDtoForView>>(categories);
+        }
+
+        public async Task<ServiceCategory> GetServiceCategoryAsync(string Id) =>  await GetById(Id);
+     
+    }
+}
