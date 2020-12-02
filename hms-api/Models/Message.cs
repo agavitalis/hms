54afe74253a280646c6d7a92815bf7cf19084c5a
Ugochukwu Udeh <ugochukwu.udeh@tenece.com>
2020-12-02 11:39:09 +0100

    Created Model used for sending mails

---
+++ hms-api/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using MimeKit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HMS.Models
+{
+    public class Message
+    {
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(x => new MailboxAddress(x,x)));
+            Subject = subject;
+            Content = content;
+        }
+        public List<MailboxAddress> To { get; }
+        public string Subject { get; }
+        public string Content { get; }
+    }
+}
