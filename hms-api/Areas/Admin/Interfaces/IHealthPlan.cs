5ebc2fb1222ddcfc50bddc93da44f711a1861cac
Ugochukwu Udeh <ugochukwu.udeh@tenece.com>
2020-12-09 08:15:34 +0100

    setup endpoint for updating and deleting healthplans

--- hms-api/Areas/Admin/Interfaces/IHealthPlan.cs
+++ hms-api/Areas/Admin/Interfaces/IHealthPlan.cs
@@ -7,6 +7,8 @@ namespace HMS.Areas.Admin.Interfaces
     public interface IHealthPlan
     {
         Task<bool> InsertHealthPlan(HealthPlan plan);
+        Task<bool> UpdateHealthPlan(HealthPlan plan);
+        Task<bool> DeleteHealthPlan(HealthPlan plan);
         Task<IEnumerable<HealthPlan>> GetAllHealthPlan();
         Task<HealthPlan> GetHealthPlanByIdAsync(string id);
     }
