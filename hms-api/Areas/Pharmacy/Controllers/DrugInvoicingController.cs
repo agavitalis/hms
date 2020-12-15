786847879c6f91e4535da7fa021d5f60a4c0f394
Ogbonna <agavitalisogbonna@gmail.com>
2020-12-15 13:46:55 +0100

    added thee pay for drugs

--- hms-api/Areas/Pharmacy/Controllers/DrugInvoicingController.cs
+++ hms-api/Areas/Pharmacy/Controllers/DrugInvoicingController.cs
@@ -119,69 +119,43 @@ namespace HMS.Areas.Pharmacy.Controllers
         }
 
 
-        //[HttpGet("GetDrugPrescription/{DrugPrescriptionId}")]
-        //public async Task<IActionResult> GetDrugPrescription(string DrugPrescriptionId)
-        //{
-        //    var drugCosting = await _drugInvoicing.GetDrugPrescription(DrugPrescriptionId);
-        //    if (drugCosting == null)
-        //    {
-        //        return BadRequest(new { message = "Invalid post attempt" });
-        //    }
+        [HttpPost("PayForDrugs")]
+        public async Task<IActionResult> PayForServices(DrugInvoicingPaymentDto drugInvoice)
+        {
+            if (drugInvoice == null)
+            {
+                return BadRequest(new { message = "Invalid Post attempt made" });
+            }
 
-        //    return Ok(new
-        //    {
-        //        drugCosting,
-        //        message = "Drug Prescription"
-        //    });
-        //}
+            //check if the patient exists
+            var patient = await _patientRepo.GetPatientByIdAsync(drugInvoice.PatientId);
+            if (patient == null)
+                return BadRequest(new
+                {
+                    response = "301",
+                    message = "This given patient could not be found",
+                });
 
+            //check if the amount is correct
+            var correctAmount = await _drugInvoicing.CheckIfAmountPaidIsCorrect(drugInvoice.InvoiceNumber, drugInvoice.TotalAmount);
+            if (correctAmount == false)
+                return BadRequest(new
+                {
+                    response = "301",
+                    message = "The Amount Paid and the Sum on the given invoice does not"
+                });
 
-        //[HttpPost("PayForPrescription")]
-        //public async Task<IActionResult> PayForServices(DrugPrescriptionPaymentDto drugs)
-        //{
-        //    if (drugs == null)
-        //    {
-        //        return BadRequest(new { message = "Invalid Post attempt made" });
-        //    }
+            //pay for drugs
+            var result = await _drugInvoicing.PayForDrugs(drugInvoice);
+            if (!result)
+                return BadRequest(new
+                {
+                    response = "301",
+                    message = "Payment for these drugs cannot be completed, pls contact the Admins"
+                });
 
-        //    //check if the patient exists
-        //    var patient = await _patientRepo.GetPatientByIdAsync(drugs.PatientId);
-        //    if (patient == null)
-        //        return BadRequest(new
-        //        {
-        //            response = "301",
-        //            message = "Invalid patient Id passed, Patient not found",
-        //        });
-
-        //    //check if all serviceRequest id passed exist
-        //    var drugPrescriptionCheck = await _drugPrescription.CheckIfDrugPrescriptionIdExists(drugs.DrugPrescriptionId);
-        //    if (!drugPrescriptionCheck)
-        //        return BadRequest(new
-        //        {
-        //            response = "301",
-        //            message = "One or more Service Request Id Passed is/are invalid"
-        //        });
-
-        //    //check if the amount is correct
-        //    var correctAmount = await _drugPrescription.CheckIfAmountPaidIsCorrect(drugs);
-        //    if (correctAmount == false)
-        //        return BadRequest(new
-        //        {
-        //            response = "301",
-        //            message = "The Amount Paid and the drug prescription paid for does not match"
-        //        });
-
-        //    //pay for services
-        //    var result = await _drugPrescription.PayForServices(drugs);
-        //    if (!result)
-        //        return BadRequest(new
-        //        {
-        //            response = "301",
-        //            message = "Payment for these drugs cannot be completed, pls contact the Admins"
-        //        });
-
-        //    return Ok(new { message = "Payment for drugs completed successfully" });
-        //}
+            return Ok(new { message = "Payment for drugs completed successfully" });
+        }
     }
 }
 
--- hms-api/Areas/Pharmacy/Interfaces/IDrugInvoicing.cs
+++ hms-api/Areas/Pharmacy/Interfaces/IDrugInvoicing.cs
@@ -14,7 +14,7 @@ namespace HMS.Areas.Pharmacy.Interfaces
         Task<IEnumerable<DrugDispensingInvoice>> GetPatientDrugInvoices(string patientId);
         Task<IEnumerable<dynamic>> GetDrugsInAnInvoice(string invoiceNumber);
         Task<bool> CheckIfAmountPaidIsCorrect(string invoiceNumber, decimal amount);
-        Task<bool> PayForDrugs(DrugInvoicingPaymentDto drugPrescription);
+        Task<bool> PayForDrugs(DrugInvoicingPaymentDto drugInvoice);
     }
 }
 
