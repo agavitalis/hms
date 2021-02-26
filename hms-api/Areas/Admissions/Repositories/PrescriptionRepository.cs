d5707163e6b9de1f666d178ad824ccc7b740566e
Ugochukwu Udeh <ugochukwu.udeh@tenece.com>
2021-02-26 12:59:14 +0100

    included doctor and patient in admission prescription

--- hms-api/Areas/Admissions/Repositories/PrescriptionRepository.cs
+++ hms-api/Areas/Admissions/Repositories/PrescriptionRepository.cs
@@ -33,7 +33,7 @@ namespace HMS.Areas.Admissions.Repositories
 
         public PagedList<PrescriptionsDtoForView> GetAdmissionPrescriptions(PaginationParameter paginationParameter)
         {
-            var prescriptions = _applicationDbContext.AdmissionPrescriptions.Include(a => a.Admission).Include(a => a.Doctor).ToList();
+            var prescriptions = _applicationDbContext.AdmissionPrescriptions.Include(a => a.Admission).ThenInclude(a => a.Patient).Include(a => a.Doctor).ToList();
             var prescriptionsToReturn = _mapper.Map<IEnumerable<PrescriptionsDtoForView>>(prescriptions);
             return PagedList<PrescriptionsDtoForView>.ToPagedList(prescriptionsToReturn.AsQueryable(), paginationParameter.PageNumber, paginationParameter.PageSize);
         }
